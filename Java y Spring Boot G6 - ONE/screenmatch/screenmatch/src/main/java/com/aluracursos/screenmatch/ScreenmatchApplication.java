8a286a6f92f36420cd682e9a45143ebcdc4995b3
Virtual Self <A01722401@tec.mx>
2024-07-01 23:19:28 -0600

    01/07 update 2

--- Java y Spring Boot G6 - ONE/screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ Java y Spring Boot G6 - ONE/screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,18 @@
 package com.aluracursos.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project1Application.class, args);
+		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hola Mundo desde Spring.");
+	}
 }
