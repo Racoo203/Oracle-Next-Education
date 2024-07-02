4fad0e1298c8b4f7430c3a0008dbdf90b7dd44a2
Virtual Self <A01722401@tec.mx>
2024-07-01 22:56:38 -0600

    01/07 update 1

---
+++ Java y Spring Boot G6 - ONE/screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+}
