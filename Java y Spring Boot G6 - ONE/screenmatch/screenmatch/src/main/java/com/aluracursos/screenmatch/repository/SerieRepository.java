384f8b5df4680bba091be33577f5bb8d1fe11ec8
Virtual Self <A01722401@tec.mx>
2024-07-11 21:30:14 -0600

    11/07 update 1

--- Java y Spring Boot G6 - ONE/screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/repository/SerieRepository.java
+++
@@ -1,8 +0,0 @@
-package com.aluracursos.screenmatch.repository;
-
-import com.aluracursos.screenmatch.models.Serie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SerieRepository extends JpaRepository<Serie, Long> {
-
-}
--- Java y Spring Boot G6 - ONE/screenmatch/screenmatch/src/main/java/com/aluracursos/screenmatch/service/IConversorDatos.java
+++
@@ -1,5 +0,0 @@
-package com.aluracursos.screenmatch.service;
-
-public interface IConversorDatos {
-    <T>T obtenerDatos(String json, Class<T> clase);
-}
